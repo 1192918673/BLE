bddf56c9a72fb2ae6fee349a188b81126c33a8d4
startimes <1192918673@qq.com>
2017-06-01 18:05:26 +0800

    util

---
+++ app/src/main/java/com/startimes/myble/util/BleUtil.java
@@ -0,0 +1,14 @@
+package com.startimes.myble.util;
+
+import android.content.Context;
+
+/**
+ * BLE Util
+ */
+
+public class BleUtil {
+
+    public static boolean isBLESupported(Context context) {
+        return false;
+    }
+}
